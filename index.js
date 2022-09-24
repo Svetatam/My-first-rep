fb4087de07af5009ac6144835869469390182c96
Sviatlana Tamashevich <tamashevichs@gmail.com>
2022-09-24 16:21:54 +0300

    Homework4

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ console.log('second dir');
 const dirOne = './second';
 fs.ensureDirSync(dirOne);
 
-// console.log('file moved');
-// fs.moveSync(file, dirOne+'/file1.txt');
+console.log('file moved');
+ fs.moveSync(file, dirOne+'/file1.txt');
 
 console.log('third dir');
 const dirThird = './third';
 fs.ensureDirSync(dirThird);
 
-// console.log('file moved');
-// fs.moveSync('./second/file1.txt', dirThird+'/file1.txt');
+console.log('file moved');
+fs.moveSync('./second/file1.txt', dirThird+'/file1.txt');
 
 console.log('file removed');
 fs.removeSync(dirThird+'/file1.txt');
