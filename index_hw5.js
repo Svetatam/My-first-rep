b5e791ad6f6effd816997eeb64882974dfa8ab9d
Sviatlana Tamashevich <tamashevichs@gmail.com>
2022-09-25 20:50:46 +0300

    Homework5

---
+++ index_hw5.js
@@ -0,0 +1,27 @@
+console.log('some string ' + true);
+console.log('string '+ 7 );
+console.log(2 + true);
+
+console.log('some string ' * true);
+console.log('string '* 7);
+console.log(2 * true);
+
+console.log('some string '/true);
+console.log('string '/7);
+console.log(2/true);
+
+let a = 22;
+let string1=a.toString();
+console.log(string1, typeof string1);
+
+let str ='50';
+let number1=Number(str);
+console.log(number1, typeof number1);
+
+let boolen=true;
+let boolen1=boolen.toString();
+console.log(boolen1, typeof boolen1);
+
+let boolen2=true;
+let boolen3=Number(boolen2);
+console.log(boolen3, typeof boolen3);
